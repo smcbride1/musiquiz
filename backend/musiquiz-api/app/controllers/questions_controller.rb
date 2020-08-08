81731afde8f35e9d67acefa2a4bc52a07ec503e1
Stephen McBride <stevemcbride3@gmail.com>
2020-08-08 01:56:52 -0400

    Added quizzes controller actions

--- backend/musiquiz-api/app/controllers/questions_controller.rb
+++ backend/musiquiz-api/app/controllers/questions_controller.rb
@@ -1,3 +1,33 @@
 class QuestionsController < ApplicationController
+    def index
+        render json: Question.all
+    end
 
+    def show
+        render json: Question.find(params[:id])
+    end
+
+    def create
+        render json: Question.create(question_params)
+    end
+
+    def update
+        render json: Question.all
+    end
+
+    def delete
+        render json: Question.all
+    end
+
+    def question_params
+        params.require(:question).permit(
+                                            :quiz_id, 
+                                            :question_type, 
+                                            :artist_name, 
+                                            :song_name, 
+                                            :youtube_url, 
+                                            :start_time, 
+                                            :answer
+                                        )
+    end
 end
--- backend/musiquiz-api/app/controllers/quizzes_controller.rb
+++ backend/musiquiz-api/app/controllers/quizzes_controller.rb
@@ -1,2 +1,21 @@
 class QuizzesController < ApplicationController
+    def show
+        render json: Quiz.find(params[:id])
+    end
+
+    def index
+        render json: Quiz.all
+    end
+
+    def create
+        Quiz.create(quizzes_params)
+    end
+
+    def delete
+        Quiz.destroy(params[:id])
+    end
+
+    def quizzes_params
+        params.require(:quizzes).permit(:user_id, :name, :custom)
+    end
 end
