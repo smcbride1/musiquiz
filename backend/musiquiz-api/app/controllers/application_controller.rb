48155a97f96fd2d1054ffe1c4a1bbe648062d88b
Stephen McBride <stevemcbride3@gmail.com>
2020-08-08 00:23:46 -0400

    Added authorize? method

--- backend/musiquiz-api/app/controllers/application_controller.rb
+++ backend/musiquiz-api/app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
-require 'billboard_api'
-
 class ApplicationController < ActionController::API
-    def top_artists
-        BillboardAPI.artist_100
+    def authorize?(user_id=nil)
+        if params.has_key?(:user_id)
+            if !session.has_key?("user_id") || params[:user_id] != session[:user_id].to_s
+                flash[:warning] = "You are not authorized to view this page."
+                return redirect_to login_path
+            end
+        else
+            if helpers.current_user.id != user_id
+                flash[:warning] = "You are not authorized to execute this action."
+                return redirect_to login_path
+            end
+        end
+        true
     end
 end
