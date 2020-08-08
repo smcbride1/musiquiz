4283b907270293e92bcfe7e71fe74302527c1c66
Stephen McBride <stevemcbride3@gmail.com>
2020-08-08 02:59:43 -0400

    Updated users_controller, added response_status method to ApplicationController

--- backend/musiquiz-api/app/controllers/application_controller.rb
+++ backend/musiquiz-api/app/controllers/application_controller.rb
@@ -2,15 +2,20 @@ class ApplicationController < ActionController::API
     def authorize?(user_id=nil)
         if params.has_key?(:user_id)
             if !session.has_key?("user_id") || params[:user_id] != session[:user_id].to_s
-                flash[:warning] = "You are not authorized to view this page."
-                return redirect_to login_path
+                return response_status(false, "You are not authorized to view this data")
             end
         else
             if helpers.current_user.id != user_id
-                flash[:warning] = "You are not authorized to execute this action."
-                return redirect_to login_path
+                return response_status(false, "You are not authorized to execute this action")
             end
         end
         true
+    end
+
+    def response_status(successful, message)
+        {
+            successful: successful,
+            message: message
+        }
     end
 end
--- backend/musiquiz-api/app/controllers/users_controller.rb
+++ backend/musiquiz-api/app/controllers/users_controller.rb
@@ -1,2 +1,50 @@
 class UsersController < ApplicationController
+    def show
+        options = {
+            include: [:name, :email, :username, :password_digest]
+        }
+        render json: UserSerializerUser.new(user, options)
+    end
+
+    def index
+        users = User.all
+        options = {
+            include: [:name, :email, :username, :password_digest]
+        }
+        render json: UserSerializerUser.new(users, options)
+    end
+
+    def create
+        if User.create(user_params)
+            response_status(true, "Successfully created user")
+        else
+            response_status(false, "Failed to create user")
+        end
+    end
+
+    def destroy
+        authorize?(params[:user_id])
+        if user.destroy
+            response_status(true, "Successfully destroyed user")
+        else
+            response_status(false, "Failed to destroy user")
+        end
+    end
+
+    def update
+        authorize?(params[:user_id])
+        if user.update(user_params)
+            response_status(true, "Successfully updated user")
+        else
+            response_status(false, "Failed to update user")
+        end
+    end
+
+    def user
+        User.find(params[:user_id])
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :email, :username, :password_digest)
+    end
 end
