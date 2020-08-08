7dd758dd0f88cd05b4814c56cab375734ed1f013
Stephen McBride <stevemcbride3@gmail.com>
2020-08-07 22:37:31 -0400

    Created top_artists route

--- backend/musiquiz-api/app/controllers/application_controller.rb
+++ backend/musiquiz-api/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
+require 'billboard_api'
+
 class ApplicationController < ActionController::API
+    def top_artists
+        BillboardAPI.artist_100
+    end
 end
--- backend/musiquiz-api/config/routes.rb
+++ backend/musiquiz-api/config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :questions_quizzes
   resources :quizzes
   resources :users
+  get "/top_artists", to: "application#top_artists"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
